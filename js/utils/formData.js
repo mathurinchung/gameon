e2cca385b1281b0676d1c77356e2181a6d20a1fe
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2022-11-09 15:37:49 -0300

    update architecture

---
+++ js/utils/formData.js
@@ -0,0 +1,39 @@
+/**
+ * Class that handles the formdata
+ */
+export default class FormData {
+  /**
+   * Representing an array of the form's elements
+   * @param {[HTMLElement]} formData - The array of the form's elements
+   */
+  constructor (formData) {
+    this.formData = formData;
+  }
+
+  /**
+   * Get FormData's element
+   * @param {String} id - selector to match
+   * @returns the formData within the document that matches the specified selector
+   */
+  getFormData = id => this.formData.find(el => el.querySelector(id));
+
+  /**
+   * Checks input
+   * @param {String} id - selector to match formdata element
+   * @param {Function} fn - The function that checks the input
+   * @returns Boolean
+   */
+  checkInput(id, fn) {
+    const formdata = this.getFormData(id);
+
+    try {
+      fn(formdata);
+
+      formdata.removeAttribute("data-error");
+      return false;
+    } catch (error) {
+      formdata.setAttribute("data-error", error.message);
+      return true;
+    }
+  }
+}
