a738965c9948686ed7acd7e454984e1673d5b9a8
Mathurin Chung <100057702+mathurinchung@users.noreply.github.com>
2023-01-05 13:54:45 -0300

    rebuild && refactoring

--- js/utils/checkIsInvalid.js
+++ js/utils/checkIsInvalid.js
@@ -1,56 +1,75 @@
-import CheckInputs from "./checkInputs.js";
+/**
+ * 
+ */
+export const schemas = {
+  letters: {
+    regex: /^[A-Za-z- ]+$/,
+    message: "Veuillez saisir un nom valide"
+  },
+  email: {
+    regex: /[a-z0-9.-_]+@[a-z0-9]+\.[a-z]{2,3}/,
+    message: "Veuillez entrer un email valide"
+  }
+};
+
+const checkRegex = (formdata, schema) => {
+  const inputValue = formdata.querySelector("input").value;
+
+  if (inputValue === "") throw new Error("Veuillez remplir ce champ");
+  else if (!schema.regex.test(inputValue)) throw new Error(schema.message);
+  else if (inputValue.length < 3) throw new Error("Veuillez saisir 3 caractères ou plus");
+};
+
+const checkBirthdate = formdata => {
+  const birthday = formdata.querySelector("input").value;
+  const dob = new Date(birthday);
+  const ageDif = Date.now() - dob.getTime();
+  const ageDate = new Date(ageDif);
+  const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+  if (birthday == null || birthday == "") throw new Error("Vous devez entrer votre date de naissance");
+  else if (age < 16) throw new Error("Vous devez avoir au moins 16 ans");
+};
+
+const checkQuantity = formdata => {
+  const quantity = formdata.querySelector("input").value;
+
+  if (quantity === "" || quantity < 0 || quantity > 99) throw new Error("Vous devez saisir un nombre entre 0 et 99");
+};
+
+const checkLocation = formdata => {
+  const location = formdata.querySelectorAll("input[name='location']:checked");
+
+  if (location.length === 0) throw new Error("Vous devez choisir une option");
+};
+
+const checkTerms = formdata => {
+  const terms = formdata.querySelector("input").checked;
+
+  if (!terms) throw new Error("Vous devez vérifier que vous acceptez les termes et conditions");
+};
 
 /**
- * Class that checks for invalid inputs
- * @extends CheckInputs
+ * 
+ * @param { string } id 
+ * @param { object } schema 
+ * @returns Boolean
  */
- export default class CheckIsInvalid extends CheckInputs {
-  /**
-   * Checks firstname is invalid
-   * @param {String} id - selector to match firstname
-   * @returns Boolean
-   */
-  firstname = id => this.checkInput(id, this.checkName);
+export const checkInput = (selector, schema) => {
+  const formElements = [ ...document.querySelectorAll(".formData") ];
+  const formData = formElements.find(el => el.querySelector(selector));
 
-  /**
-   * Checks lastname is invalid
-   * @param {String} id - selector to match lastname
-   * @returns Boolean
-   */
-  lastname = id => this.checkInput(id, this.checkName);
+  try {
+    if (selector === "#first" || selector === "#last" || selector === "#email") checkRegex(formData, schema);
+    else if (selector === "#birthdate") checkBirthdate(formData)
+    else if (selector === "#quantity") checkQuantity(formData);
+    else if (selector === "input[name='location']") checkLocation(formData);
+    else if (selector === "#checkbox1") checkTerms(formData);
 
-  /**
-   * Checks email is invalid
-   * @param {String} id - selector to match email
-   * @returns Boolean
-   */
-  email = id => this.checkInput(id, this.checkEmail);
-
-  /**
-   * Checks birthdate is invalid
-   * @param {String} id - selector to match birthdate
-   * @returns Boolean
-   */
-  birthdate = id => this.checkInput(id, this.checkBirthdate);
-
-  /**
-   * Checks quantity is invalid
-   * @param {String} id - selector to match quantity
-   * @returns Boolean
-   */
-  quantity = id => this.checkInput(id, this.checkQty);
-
-  /**
-   * Checks location is invalid
-   * @param {String} id - selector to match location
-   * @returns Boolean
-   */
-  location = id => this.checkInput(id, this.checkOption);
-
-  /**
-   * Checks terms is invalid
-   * @param {String} id - selector to match terms
-   * @returns Boolean
-   */
-  terms = id => this.checkInput(id, this.checkTerms);
-}
+    formData.removeAttribute("data-error");
+    return false;
+  } catch (error) {
+    formData.setAttribute("data-error", error.message);
+    return true;
+  }
+};
